b6cbe35745ee7d7f77e79383a4a5723064e5d2ce
JianyuYang <yztxwd@gmail.com>
2022-02-03 23:48:00 -0500

    Minor fix

--- construct_data/utils.py
+++ construct_data/utils.py
@@ -287,7 +287,8 @@ def get_data_TFRecord(coords, genome_fasta, chromatin_tracks, nbins, outprefix, 
     genome_pyfasta = pyfasta.Fasta(genome_fasta)
 
     # split coordinates and assign chunks to workers
-    num_chunks = math.ceil(len(coords) / 7000)
+    #num_chunks = math.ceil(len(coords) / 7000)
+    num_chunks = len(coords) 
     chunks = np.array_split(coords, num_chunks)
     get_data_TFRecord_worker_freeze = functools.partial(get_data_TFRecord_worker, 
                                                     fasta=genome_pyfasta, nbins=nbins, 
